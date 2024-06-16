47596ea8fd1a24d9ac980a3a43e44ccf431f6f49
Sion <solee9802@gmail.com>
2024-06-17 00:17:14 +0900

    home화면 퍼블리싱

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,38 @@
-import './App.css'
+import './reset.css'
+import './App.scss'
 
 function App() {
 
   return (
     <>
       <div className="App">
-        <h1>Hello, Bootstrap!</h1>
-        <button className="btn btn-primary">Bootstrap Button</button>
+        <div className='title'>BeauLine</div>
+        <div className='info'>
+          안녕하세요 <span>이시온</span> 님
+        </div>
+        <div className="row row-cols-2 text-center">
+          <button className="navBtn">
+            <a href='#'>주소록</a>
+          </button>
+          <button className="navBtn">
+            <a href='#'>문자</a>
+          </button>
+          <button className="navBtn">
+            <a href='#'>작업 카테고리</a>
+          </button>
+          <button className="navBtn">
+            <a href='#'>작업 내역</a>
+          </button>
+          <button className="navBtn">
+            <a href='#'>작업 후 처리 예정</a>
+          </button>
+          <button className="navBtn">
+            <a href='#'>통계 차트</a>
+          </button>
+        </div>
     </div>
     </>
   )
 }
 
-export default App
+export default App;
