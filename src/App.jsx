a71f716f9d4a56e53301f2040360c8302de0c636
Sion <solee9802@gmail.com>
2024-06-16 20:39:56 +0900

    프로젝트 생성

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="App">
+        <h1>Hello, Bootstrap!</h1>
+        <button className="btn btn-primary">Bootstrap Button</button>
+    </div>
+    </>
+  )
+}
+
+export default App
