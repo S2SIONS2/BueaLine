2fd508b6cbf69034a66345c790c29c67c6cf38d9
Sion <solee9802@gmail.com>
2024-06-30 18:46:17 +0900

    카테고리 초기화 및 api 재연결

--- src/components/CategoryList.jsx
+++ src/components/CategoryList.jsx
@@ -2,30 +2,106 @@ import './CategoryList.scss';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 
-const CategoryList = ({ valueArray }) => {
+const CategoryList = () => {
+    const [modal, setModal] = useState(false); // modal state
     const [list, setList] = useState([]); // api 리스트
-
+    const [categoryInput, setCategoryInput] = useState({
+        category_name: '',
+        category_price: ''
+      });
+    
+    // input value 등록
+    const onChangeValue = (e) => {
+        const { name, value } = e.target;
+        setCategoryInput({
+        ...categoryInput,
+        [name]: value
+        });
+    }
+    // api list 가져옴
     const apiList = async () => {
         const url = '/getList';
         const response = await axios.get(url);
         return response.data;
     };
 
-    useEffect(() => {
-        const getList = async () => {
-            try {
-                const data = await apiList();
-                const listFromApi = data.list;
-                setList([...valueArray, ...listFromApi]);
-            } catch (error) {
-                console.error(`Error list:`, error);
-            }
-        };
-        getList();
-    }, [valueArray]);
+    const getList = async () => {
+        try {
+            const data = await apiList();
+            const listFromApi = data.list;
+            setList(listFromApi);
+        } catch (error) {
+            console.error(`Error list:`, error);
+        }
+    };
 
+    useEffect(() => {
+        getList()
+    }, []); 
+
+    // modal open
+    const openModal = () => {
+        setModal(true);
+    }
+
+    // 확인 버튼 클릭 시
+    const addedApi = async () => {
+        const category_name = categoryInput.category_name;
+        const category_price = categoryInput.category_price;
+
+        if (category_name == '') {
+            alert('error!');
+            return false;
+        }
+        if (!category_price) {
+            alert('error!');
+            return false;
+        }
+
+        let params = {
+            category_name: category_name,
+            category_price: category_price,
+            category_sort: 0,
+            category_cha: 1,
+            parent_id: 0
+        }
+        const url = '/add';
+        const response = await axios.post(url, params);
+        console.log(response.data.code)
+       
+        if (response.data.code === 200){
+            getList()
+            category_name.delete()
+            setModal(false)
+        }
+    }
     return (
         <div className="CategoryList">
+            <div className='row align-items-center justify-content-end row-cols-4' style={{ width: '100%' }}>
+                <button 
+                    className='openModal btn btn-secondary' type='button'
+                    onClick={openModal}
+                >
+                    카테고리 추가
+                </button>    
+            </div>
+            {
+                modal &&
+                    <div className='modalContainer'>
+                        <div className='modalContent'>
+                            <div className='addCategoryArea'>
+                                시술명: <input type='text' name='category_name' value={categoryInput.category_name} onChange={onChangeValue}/>
+                                금액: <input type='number' name='category_price' value={categoryInput.category_price} onChange={onChangeValue}/>
+                            </div>
+                            <div className='btn_wrap '>
+                                <button className='btn-point-dark btn_small' 
+                                    onClick={addedApi}>
+                                    확인</button>
+                                <button className='btn-point-dark btn_small' onClick={() => setModal(false)}>닫기</button>
+                            </div>
+                        </div>
+                    </div>
+            }
             <div className='row flex-column'>
                 <div className='row row-cols-2 align-items-center th'>
                     <div className='text-center'>시술 명</div>
--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,170 +1,17 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
+// import { useEffect, useState } from 'react';
+// import axios from 'axios';
 import './Category.scss';
 import CategoryList from '../components/CategoryList';
 
 const Category = () => {
-    // 모달 생성
-    const [modal, setModal] = useState(false);
-    const openModal = () => {
-        setModal(true);
-    };
-
-    // 카테고리 기본 설정
-    const [inputValue, setInputValue] = useState([{ category_name: '', category_price: '' }]);
-    
-    // 카테고리 추가 시
-    const addCategory = () => {
-        setInputValue([...inputValue, { category_name: '', category_price: '' }]);
-    };
-
-    // 카테고리 삭제
-    // const deleteCategory = (index) => {
-    //     const newInputValues = inputValue.filter((_, i) => i !== index);
-    //     setInputValue(newInputValues);
-    // };
-
-    const handleChange = (index, e) => {
-        const { name, value } = e.target;
-        const newInputValues = [...inputValue];
-        newInputValues[index] = { ...newInputValues[index], [name]: value };
-        setInputValue(newInputValues);
-    };
-
-    const [valueArray, setValueArray] = useState([]);
-
-    const categoryConfirm = async () => {
-        try {
-            if (inputValue.every(item => item.category_name !== '' && item.category_price !== '')) {
-                for (const item of inputValue) {
-                    await addApi(item);
-                }
-                setValueArray(inputValue);
-                setModal(false);
-            } else if (inputValue.every(item => item.category_name !== '')) {
-                alert('비용을 입력 해주세요.');
-            } else if (inputValue.every(item => item.category_price !== '')) {
-                alert('시술 명을 입력 해주세요.');
-            } else {
-                setModal(false);
-            }
-        } catch (error) {
-            console.log(`Error occured:`, error);
-        }
-    };
-
-    const addApi = async (item) => {
-        const url = '/add';
-        const params = {
-            parent_id: 0,
-            category_name: item.category_name,
-            category_price: item.category_price,
-            category_sort: 0,
-            category_cha: 1,
-        };
-        const response = await axios.post(url, params);
-        return response.data;
-    };
-
-    useEffect(() => {
-        const fetchInitialData = async () => {
-            try {
-                const response = await axios.get('/getList');
-                setValueArray(response.data.list);
-            } catch (error) {
-                console.log('Error Occured:', error);
-            }
-        };
-        fetchInitialData();
-    }, []);
-
-    
-
     return (
         <div className='Category'>
             <div className='subTitle'>Category</div>
-            <div className='row align-items-center justify-content-end row-cols-4' style={{ width: '100%' }}>
-                <button 
-                    className='openModal btn btn-secondary' type='button'
-                    onClick={openModal}
-                >
-                    카테고리 추가
-                </button>    
-            </div>
             <section>
-                <CategoryList valueArray={valueArray} setValueArray={setValueArray}/>
+                <CategoryList/>
             </section>
 
-            {
-                modal && 
-                <article className='modalContainer'>
-                    <div className='modalHeader'>카테고리 추가</div>
-                    <div className='modalContent'>
-                        <div className='row align-items-center justify-content-end row-cols-4' style={{ '--bs-gutter-x': '0px' }}>
-                            <button 
-                                className='btn btn-point-dark' type='button'
-                                onClick={addCategory}
-                            >
-                                항목 추가
-                            </button>
-                        </div>
-                        <div className='addCategory'>
-                            {
-                                inputValue.map((item, index) => (
-                                    <div 
-                                        className='categoryWrap'
-                                        key={index}
-                                    >
-                                        {/* <div className='btn_wrap'>
-                                            <button className='btn btn-point-dark' type='button' onClick={() => deleteCategory(index)}>삭제</button>
-                                        </div> */}
-                                        <div className='row align-items-center'>
-                                            <div className='col-4'>시술 명</div>
-                                            <div className='col-8'>
-                                                <input
-                                                    className='col-12' 
-                                                    type='text'
-                                                    name='category_name'
-                                                    value={item.category_name}
-                                                    onChange={(e) => handleChange(index, e)}
-                                                />
-                                            </div>
-                                        </div>
-                                        <div className='row row-cols-2 align-items-center'>
-                                            <div className='col-4'>금액</div>
-                                            <div className='col-8'>
-                                                <input
-                                                    className='col-12'
-                                                    type='number'
-                                                    name='category_price'
-                                                    value={item.category_price}
-                                                    onChange={(e) => handleChange(index, e)}
-                                                />
-                                            </div>
-                                        </div>
-                                    </div>
-                                ))
-                            }
-                        </div>
-                        <div className='btn_wrap'>
-                            <button 
-                                type='button' 
-                                className='btn btn-point-dark'
-                                onClick={categoryConfirm}
-                            >
-                                확인
-                            </button>
-                            <button 
-                                type='button' 
-                                className='btn btn-light'
-                                onClick={() => setModal(false)}
-                            >
-                                닫기
-                            </button>
-                        </div>
-                    </div>
-                </article>
-            }
+
         </div>
     );
 };
